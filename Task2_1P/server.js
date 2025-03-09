cbba6cb5e526c8687240062bec54415b8ef0ee85
shricenguyen <amborse31@gmail.com>
2025-03-09 15:55:17 +1100

    first commit 2.1P

---
+++ Task2_1P/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const addTwoNumbers = (a, b) => {
+  return a + b;
+};
+
+app.get("/addTwoNumbers", (req, res) => {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+
+  if (isNaN(a) || isNaN(b)) {
+    return res.status(400).json({
+      message: "Please enter a number",
+    });
+  }
+  const result = addTwoNumbers(a, b);
+  res.json({
+    status: 200,
+    data: result,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
